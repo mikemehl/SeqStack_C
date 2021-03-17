eed2a2cf2d7d756744b7182a2a2592b206bee9eb
mikemehl <michaelalanmehl@gmail.com>
2021-03-17 16:37:05 -0400

    Test tweaks, ISA notes.

--- test/src/main.c
+++ test/src/main.c
@@ -19,6 +19,7 @@ test_return_type stack_test()
    VM_Stack stk;
    seqstk_stk_init(&stk);
    test_assert((stk.top == -1), "STACK INIT: TOP != -1");
+   test_assert(seqstk_stk_empty(&stk), "STACK NOT EMPTY AFTER INIT!");
 
    bool is_empty = seqstk_stk_empty(&stk);
    test_assert((is_empty), "EMPTY TEST FAILED!");
@@ -26,9 +27,9 @@ test_return_type stack_test()
    int32_t const * const empty_ret = seqstk_stk_peek(&stk);
    test_assert((empty_ret == NULL), "PEEK RETURNED A VALUE FROM EMPTY STACK!");
 
-   seqstk_stk_push(&stk, 4);
-   seqstk_stk_push(&stk, 5);
-   seqstk_stk_push(&stk, 6);
+   test_assert(seqstk_stk_push(&stk, 4), "PUSH FAILED!");
+   test_assert(seqstk_stk_push(&stk, 5), "PUSH FAILED!");
+   test_assert(seqstk_stk_push(&stk, 6), "PUSH FAILED!");
    test_assert((stk.data[stk.top] == 6), "STACK PUSH FAILED! TOP ITEM INCORRECT!");
    test_assert((stk.data[stk.top - 1] == 5), "STACK PUSH FAILED! SECOND ITEM INCORRECT!");
    
@@ -46,6 +47,13 @@ test_return_type stack_test()
    test_assert(*pop_ret == 4, "POPPED WRONG VALUE, NOT 4!!!");
    pop_ret = seqstk_stk_pop(&stk);
    test_assert(pop_ret == NULL, "POP RETURNED NONNULL ON EMPTY STACK!");
+
+   // Test for trying to push to a full stack (should fail).
+   stk.top = STACK_SIZE;
+   test_assert(!seqstk_stk_push(&stk, 666), "STACK OVERFLOW NOT CHECKED!");
+
+   // Reinitialize.
+   seqstk_stk_init(&stk);
 
    return_test_success;
 }
--- test/src/test.h
+++ test/src/test.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 #define test_assert(test, message) do { if (!(test)) return message; } while(0)
-#define test_run(test) do { char const *message = test(); if (message != NULL) { printf("%s\n", "FAILED TEST: "); printf("%s\n", message); return -1; }} while(0)
+#define test_run(test) do { char const *message = test(); if (message != NULL) { printf("%s\n", "FAILED TEST: "); printf("\t%s\n", message); return -1; }} while(0)
 #define test_return_type char *
 #define return_test_success return NULL
 
